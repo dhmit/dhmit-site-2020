9074a21ed1043cff809c1958475e5eda2f786b38
mikehwagz <mike@selfaware.studio>
2020-03-22 13:15:02 -0400

    add comments in overlay transition

--- web/src/scripts/transitions/overlay.js
+++ web/src/scripts/transitions/overlay.js
@@ -3,10 +3,20 @@ import app from '@/app'
 import gsap from 'gsap'
 
 class Overlay extends Highway.Transition {
-  in({ from, to, done }) {
-    const toView = to.dataset.routerView
+  // because our modal needs to overlap the landing page, we
+  // need to skip the OUT transition hook and handle all animations
+  // during the IN transition hook which occurs after the new page
+  // html has been fetched and added to the dom
+  out({ done }) {
+    done()
+  }
 
-    if (toView === 'modal') {
+  in({ from, to, done }) {
+    // check if the new page is a 'modal' or not
+    const isDestinationModal = to.dataset.routerView === 'modal'
+
+    // if it is, we need to animate in the modal
+    if (isDestinationModal) {
       const inner = to.querySelector('.js-inner')
       gsap.fromTo(
         to,
@@ -31,29 +41,31 @@ class Overlay extends Highway.Transition {
           ease: 'quart',
           onComplete: () => {
             done()
+            // remove previous page container
             from.remove()
           },
         },
       )
     } else {
+      // restore previous landing page scroll destination to create the illusion
+      // that the modal was just an overlay on top of the previous page
       const { windowHeight, percentScrolled } = app.getState()
+
       to.scrollTop =
         (to.firstElementChild.offsetHeight - windowHeight) * percentScrolled
 
+      // fade out the modal
       gsap.to(from, {
         duration: 0.3,
         autoAlpha: 0,
         ease: 'cubic',
         onComplete: () => {
           done()
+          // remove previous page container
           from.remove()
         },
       })
     }
-  }
-
-  out({ done }) {
-    done()
   }
 }
 
