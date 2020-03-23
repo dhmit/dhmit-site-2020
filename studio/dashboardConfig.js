bd21c4bec8f7625ad212154353bcffd60ea3e5c9
mikehwagz <mike@selfaware.studio>
2020-03-23 14:55:58 -0400

    feat(studio): add sanity dashboard and netlify widget integration

---
+++ studio/dashboardConfig.js
@@ -0,0 +1,18 @@
+export default {
+  widgets: [
+    {
+      name: 'netlify',
+      options: {
+        title: 'Netlify',
+        sites: [
+          {
+            title: 'Website',
+            apiId: '5cccb8c2-5382-4b5e-9f03-19cb6ede805c',
+            buildHookId: '5e7904df232a7316c642dc86',
+            name: 'dhmit',
+          },
+        ],
+      },
+    },
+  ],
+}
