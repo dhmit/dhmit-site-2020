80374d4b6d2186fd7e16a34a2aa0b54ba960c955
mikehwagz <mike@selfaware.studio>
2020-03-24 13:13:40 -0400

    refactor(web): remove unnecessary dom ready wrapper

--- web/src/scripts/index.js
+++ web/src/scripts/index.js
@@ -1,33 +1,32 @@
 import 'focus-visible'
 import app from '@/app'
 import router from '@/router'
-import { ready, on, size } from '@/util/dom'
+import { on, size } from '@/util/dom'
 import gsap from 'gsap'
 
-ready(() => {
-  // Broadcast window resize events
-  on(window, 'resize', resize)
+// Broadcast window resize events
+on(window, 'resize', resize)
 
-  // Setup global raf loop
-  gsap.ticker.add(tick)
+// Setup global raf loop
+gsap.ticker.add(tick)
 
-  // Mount picoapp
+// Mount picoapp
+app.mount()
+
+// Propagate an initial resize event
+resize()
+
+// Listen for page transitions and mount new components
+router.on('NAVIGATE_IN', () => {
+  app.unmount()
   app.mount()
-
-  // Propagate an initial resize event
   resize()
-
-  router.on('NAVIGATE_IN', () => {
-    app.unmount()
-    app.mount()
-    resize()
-  })
-
-  function resize() {
-    app.emit('resize', size())
-  }
-
-  function tick() {
-    app.emit('tick')
-  }
 })
+
+function resize() {
+  app.emit('resize', size())
+}
+
+function tick() {
+  app.emit('tick')
+}
