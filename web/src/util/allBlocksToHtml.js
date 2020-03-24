1f78015c09153eea0d8f93b99468acf9108def78
mikehwagz <mike@selfaware.studio>
2020-03-24 08:45:05 -0400

    fix(web): edit block serializer so links in richText open in a new tab

--- web/src/util/allBlocksToHtml.js
+++ web/src/util/allBlocksToHtml.js
@@ -1,9 +1,28 @@
 const blocksToHtml = require(`@sanity/block-content-to-html`)
 
+const h = blocksToHtml.h
+const serializers = {
+  marks: {
+    link: (props) =>
+      h(
+        'a',
+        {
+          href: props.mark.href,
+          target: '_blank',
+          rel: 'noopener noreferrer',
+        },
+        props.children,
+      ),
+  },
+}
+
 module.exports = function allBlocksToHtml(data) {
   if (typeof data === 'object') {
     if (data._type && data._type === 'richText') {
-      return blocksToHtml(data)
+      return blocksToHtml({
+        blocks: data.blocks,
+        serializers,
+      })
     } else {
       const entries = Object.entries(data)
       for (let i = 0; i < entries.length; i++) {
